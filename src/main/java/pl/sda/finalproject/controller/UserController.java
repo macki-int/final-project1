f27e6be205ee1b1e6fbf48ec0a694ef3bbaf6b2e
mjanicki <macki@int.pl>
2019-04-18 15:54:42 +0200

    add methods getAdmins, getUsers, getViewer

--- src/main/java/pl/sda/finalproject/controller/UserController.java
+++ src/main/java/pl/sda/finalproject/controller/UserController.java
@@ -1,8 +1,15 @@
 package pl.sda.finalproject.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.finalproject.model.User;
 import pl.sda.finalproject.repository.UserRepository;
+
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -13,5 +20,31 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @GetMapping
+    public List<User> get() {
+        return userRepository.findAll();
+    }
 
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/admins")
+    public List<User> getAdmins() {
+        return userRepository.findAll();
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        //TODO
+        //return userRepository.findAll();
+        return null;
+    }
+
+    @Secured("ROLE_VIEWER")
+    @GetMapping("/viewers")
+    public User getViewer(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException());
+        //TODO - sprawdzić jak i dlaczego
+    }
 }
--- src/main/java/pl/sda/finalproject/model/User.java
+++ src/main/java/pl/sda/finalproject/model/User.java
@@ -44,7 +44,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     @Override
