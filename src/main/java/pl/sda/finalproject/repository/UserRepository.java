836ce6e37f79801fdc94f3de88c091f563d48cf0
macki <macki@int.pl>
2019-04-18 02:16:05 +0200

    added User repository

--- src/main/java/pl/sda/finalproject/repository/UserRepository.java
+++ src/main/java/pl/sda/finalproject/repository/UserRepository.java
@@ -3,6 +3,8 @@ package pl.sda.finalproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sda.finalproject.model.User;
 
-public interface UserRepository extends JpaRepository <User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/pl/sda/finalproject/service/UserDetailsServiceImpl.java
@@ -0,0 +1,16 @@
+package pl.sda.finalproject.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.sda.finalproject.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException(username));
+    }
+}
