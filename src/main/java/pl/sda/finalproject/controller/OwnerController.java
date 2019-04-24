609ad59e08f6a69a957cc599caec8d4379e0baff
mjanicki <macki@int.pl>
2019-04-24 15:13:51 +0200

    modified controllers for owner and apartment

--- src/main/java/pl/sda/finalproject/controller/OwnerController.java
+++ src/main/java/pl/sda/finalproject/controller/OwnerController.java
@@ -1,7 +1,10 @@
 package pl.sda.finalproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.finalproject.model.Owner;
 import pl.sda.finalproject.repository.OwnerRepository;
 
 @RestController
@@ -11,5 +14,11 @@ public class OwnerController {
 
     public OwnerController(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
+    }
+
+    @GetMapping("/{id}")
+    public Owner get(@PathVariable long id){
+        return ownerRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Owner is not exist in DB"));
     }
 }
